37effe098509b583788f01fe243e829d2510a76a
Buga Dániel <daniel@bugadani.hu>
2014-01-23 17:56:22 +0100

    Initial commit.

---
+++ WorkerController.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of the Miny framework.
+ * (c) Dániel Buga <daniel@bugadani.hu>
+ *
+ * For licensing information see the LICENSE file.
+ */
+
+namespace Modules\CLI;
+
+use Miny\Controller\BaseController;
+
+abstract class WorkerController extends BaseController
+{
+
+}
